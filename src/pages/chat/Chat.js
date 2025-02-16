f2c07f526edf8843ad5be95b1768a36d5b58349a
Zaheer <za00901@gmail.com>
2025-02-17 01:38:26 +0500

    fix

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { Layout, message } from 'antd';
 import ChatSidebar from '../../components/chat/ChatSidebar';
 import ChatHeader from '../../components/chat/ChatHeader';
